9fe40211423f20db58608b25a52c4bbb9aeb5077
Joao <joao.santos75@fatec.sp.gov.br>
2017-12-02 01:27:05 -0200

    First Commit

---
+++ backend/config/routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+module.exports = function(server){
+
+  // API routes (rotas)
+  const router = express.Router()
+  server.use('/api', router)
+
+  // rotas da API
+  const billingCycleService = require('../api/billingCycle/billingCycleService')
+  billingCycleService.register(router, '/billingCycle')
+
+  const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+  router.route('/billingSummary').get(billingSummaryService.getSummary)
+}
---
+++ backend/config/server.js
@@ -0,0 +1,26 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extend: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function(){
+  console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
+
+/*
+server.use(function(req, res, next){
+  console.log('meu middleware 1');
+  next()
+})
+
+server.use(function(req, res, next){
+  console.log('meu middleware 2');
+  res.send('Funcionou novamente....')
+})
+*/
