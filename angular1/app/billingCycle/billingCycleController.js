3e7464a4a72227f7713210215430f7213407e9af
João Pedro Chér <joao.santos75@fatec.sp.gov.br>
2017-12-08 19:56:29 -0200

    commit OK! *warn

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,17 +1,19 @@
 (function() {
   angular.module('primeiraApp').controller('billingCycleCtrl', [
     '$http',
+    'msgs',
     billingCycleCtrl
   ])
 
-  function billingCycleCtrl($http) {
+  function billingCycleCtrl($http, msgs) {
     const vm = this
     vm.create = function() {
-      const url = 'http://localhost:3003/api/billingCycles'
+      const url = 'http://localhost:3003/api/billingCycle'
       $http.post(url, vm.billingCycle).then(function(response) {
-        vm.billingCycle = {}
-        console.log('Sucesso!');
-      })
+            msgs.addSuccess('Operação realizada com Sucesso!')
+         }).catch(function(response) {
+            msgs.addError(response.data.errors)
+         })
     }
   }
 })()
--- backend/api/billingCycle/billingCycleService.js
+++ backend/api/billingCycle/billingCycleService.js
@@ -1,5 +1,5 @@
-const BillingCycle = require('./billingCycle')
 const _ = require('lodash')
+const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
