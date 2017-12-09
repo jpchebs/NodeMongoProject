87a6e5f6ad365e33754517f40251e0783221768e
João Pedro Chér <joao.santos75@fatec.sp.gov.br>
2017-12-09 00:23:51 -0200

    commit OK

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -2,18 +2,31 @@
   angular.module('primeiraApp').controller('billingCycleCtrl', [
     '$http',
     'msgs',
+    'tabs',
     billingCycleCtrl
   ])
 
-  function billingCycleCtrl($http, msgs) {
+  function billingCycleCtrl($http, msgs, tabs) {
     const vm = this
+    const url = 'http://localhost:3003/api/billingCycle/'
+
+    vm.refresh = function() {
+      $http.get(url).then(function(response) {
+        vm.billingCycle = {}
+        vm.billingCycles = response.data
+        tabs.show(vm, {tabList: true, tabCreate: true})
+      })
+    }
+
     vm.create = function() {
-      const url = 'http://localhost:3003/api/billingCycle'
       $http.post(url, vm.billingCycle).then(function(response) {
+            vm.refresh()
             msgs.addSuccess('Operação realizada com Sucesso!')
          }).catch(function(response) {
             msgs.addError(response.data.errors)
          })
     }
+
+    vm.refresh()
   }
 })()
