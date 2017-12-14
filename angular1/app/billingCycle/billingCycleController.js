651d998caed11b85bf18dbd5d5689380434516d8
Joao <joao.santos75@fatec.sp.gov.br>
2017-12-14 00:54:30 -0200

    --

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -10,18 +10,25 @@
     const vm = this
     const url = 'http://localhost:3003/api/billingCycle'
 
+    // Função para manter os objeto atualizados na tela
     vm.refresh = function() {
       $http.get(url).then(function(response) {
-        vm.billingCycle = {}
+        vm.billingCycle = {credits: [{}], debts: [{}]}
         vm.billingCycles = response.data
         tabs.show(vm, {tabList: true, tabCreate: true})
       })
     }
 
+    // Função para persistir um novo objeto
     vm.create = function() {
+      // Acionando o service HTTP passando o método put
+      // Passa a url ja definida e o objeto 'billingCycle' 'set' em showTabUpdate
       $http.post(url, vm.billingCycle).then(function(response) {
+            // Zera os objetos e retorna para lista de objetos
             vm.refresh()
+            // Exibindo mensagem de sucesso
             msgs.addSuccess('Operação realizada com Sucesso!')
+            // Exibindo mensagem de erro(s) caso nao seja sucesso
          }).catch(function(response) {
             msgs.addError(response.data.errors)
          })
@@ -45,7 +52,7 @@
         msgs.addSuccess('Operação realizada com Sucesso!')
         // Exibindo mensagem de erro(s) caso nao seja sucesso
       }).catch(function(response) {
-        msgs.addError(response.data.error)
+        msgs.addError(response.data.errors)
       })
     }
 
@@ -54,14 +61,60 @@
       tabs.show(vm, {tabDelete: true})
     }
 
+    // Função para deletar objeto
     vm.delete = function() {
+      // Definindo a url ja criada acima e concatenando com a url do objeto requerido
       const deleteUrl = `${url}/${vm.billingCycle._id}`
+      // Acionando o service HTTP passando o método put
+      // Passa a url ja definida e o objeto 'billingCycle' 'set' em showTabUpdate
       $http.delete(deleteUrl, vm.billingCycle).then(function(response) {
+        // Zera os objetos e retorna para lista de objetos
         vm.refresh()
+        // Exibindo mensagem de sucesso
         msgs.addSuccess('Operação realizada com Sucesso!')
+        // Exibindo mensagem de erro(s) caso nao seja sucesso
       }).catch(function(response) {
          msgs.addError(response.data.errors)
       })
+    }
+
+    // Método referenciado pelo controller, para ser visivel externamente
+    // Passando o index como parâmetro
+    vm.addCredit = function(index) {
+      // método splice altera o array atual, passando o indice, o núm. de elementos
+       // para remover e a lista de itens a ser adicionada
+      vm.billingCycle.credits.splice(index + 1, 0, {}) // Passando objeto vazio {}
+    }
+
+    // Passando index no parâmetro, nome e valor estão como obj
+    vm.cloneCredit = function(index, {name, value}) {
+      vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+    }
+
+    vm.deleteCredit = function(index) {
+      // Por regra da aplicação, deve haver pelo menos um elemento
+      // O tamanho do array (lista) de creditos deve ser maior que 1
+      console.log('antes');
+      if (vm.billingCycle.credits.lenght > 1) {
+        console.log('dentro');
+        // Passando o indice e a quantidade de elementos a serem excluidos
+        vm.billingCycle.credits.splice(index, 1)
+      }
+      console.log('depois');
+    }
+
+    vm.addDebt = function(index) {
+      vm.billingCycle.debts.splice(index + 1, 0, {})
+    }
+
+    vm.cloneDebt = function(index, {name, value, status}) {
+      vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+    }
+
+    vm.deleteDebt = function(index) {
+      if (vm.billingCycle.debts.lenght > 1) {
+        vm.billingCycle.debts.splice(index, 1)
+      }
     }
 
     vm.refresh()
