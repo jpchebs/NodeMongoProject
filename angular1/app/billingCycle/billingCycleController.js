45971fa2cb9d1bc9719634944e4d9f964c7a233a
João Pedro Chér <joao.santos75@fatec.sp.gov.br>
2017-12-15 03:10:29 -0200

    commit saved-- all ok

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,23 +1,34 @@
 (function() {
   angular.module('primeiraApp').controller('billingCycleCtrl', [
     '$http',
+    '$location',
     'msgs',
     'tabs',
     billingCycleCtrl
   ])
 
-  function billingCycleCtrl($http, msgs, tabs) {
+  function billingCycleCtrl($http, $location, msgs, tabs) {
     const vm = this
     const url = 'http://localhost:3003/api/billingCycle'
 
     // Função para manter os objeto atualizados na tela
     vm.refresh = function() {
-      $http.get(url).then(function(response) {
+      const page = parseInt($location.search().page) || 1
+      $http.get(`${url}?skip=${(page - 1) * 10}&limit=10`).then(function(response) {
         vm.billingCycle = {credits: [{}], debts: [{}]}
         vm.billingCycles = response.data
+        vm.calculateValues()
         tabs.show(vm, {tabList: true, tabCreate: true})
+
+        // Retorna a quantiade de registros que existem na 'collection'
+        $http.get(`${url}/count`).then(function(response) {
+          vm.pages = Math.ceil(response.value / 10)
+          // console.log('pages =', vm.pages) -> retornando NaN* (deveria retornar o valor de quantidade)
+        })
       })
     }
+
+// ======================================*CRUD========================================================
 
     // Função para persistir um novo objeto
     vm.create = function() {
@@ -34,8 +45,10 @@
          })
     }
 
+    // Habilitando a aba de Alterar elemento, quando necessária
     vm.showTabUpdate = function(billingCycle) {
       vm.billingCycle = billingCycle
+      vm.calculateValues()
       tabs.show(vm, {tabUpdate: true})
     }
 
@@ -56,8 +69,10 @@
       })
     }
 
+    // Habilitando a aba de Excluir elemento, quando necessária
     vm.showTabDelete = function(billingCycle) {
       vm.billingCycle = billingCycle
+      vm.calculateValues()
       tabs.show(vm, {tabDelete: true})
     }
 
@@ -78,6 +93,8 @@
       })
     }
 
+// ===================================*bCycle===========================================================
+
     // Método referenciado pelo controller, para ser visivel externamente
     // Passando o index como parâmetro
     vm.addCredit = function(index) {
@@ -89,6 +106,7 @@
     // Passando index no parâmetro, nome e valor estão como obj
     vm.cloneCredit = function(index, {name, value}) {
       vm.billingCycle.credits.splice(index + 1, 0, {name, value})
+      vm.calculateValues()
     }
 
     vm.deleteCredit = function(index) {
@@ -97,21 +115,55 @@
       if (vm.billingCycle.credits.length > 1) {
         // Passando o indice e a quantidade de elementos a serem excluidos
         vm.billingCycle.credits.splice(index, 1)
+        vm.calculateValues()
       }
     }
 
+    // Método referenciado pelo controller, para ser visivel externamente
+    // Passando o index como parâmetro
     vm.addDebt = function(index) {
-      vm.billingCycle.debts.splice(index + 1, 0, {})
+      // método splice altera o array atual, passando o indice, o núm. de elementos
+       // para remover e a lista de itens a ser adicionada
+      vm.billingCycle.debts.splice(index + 1, 0, {}) // Passando objeto vazio {}
     }
 
+    // Passando index no parâmetro, nome e valor estão como obj
     vm.cloneDebt = function(index, {name, value, status}) {
       vm.billingCycle.debts.splice(index + 1, 0, {name, value, status})
+      vm.calculateValues()
     }
 
     vm.deleteDebt = function(index) {
+      // Por regra da aplicação, deve haver pelo menos um elemento
+      // O tamanho do array (lista) de creditos deve ser maior que 1
       if (vm.billingCycle.debts.length > 1) {
+        // Passando o indice e a quantidade de elementos a serem excluidos
         vm.billingCycle.debts.splice(index, 1)
+        vm.calculateValues()
       }
+    }
+
+    // Função para calcular e mostrar os valores de créditos e débitos na tela de novo ciclo
+    vm.calculateValues = function() {
+      vm.credit = 0
+      vm.debt = 0
+
+      // Verificar se o valor atribuido é válido
+      if (vm.billingCycle) {
+        // Passando por todos os elementos de Créditos
+        vm.billingCycle.credits.forEach(function({value}) {
+          // Atribuição aditiva, se o valor(value) não existir ou for Not a Number(NaN), retorna 0 como padrão
+          // Se o valor existir e é um número, usa parseDouble e coloca na variavel
+          vm.credit += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+        vm.billingCycle.debts.forEach(function({value}) {
+          vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+      }
+
+      // Atribuindo a variavel 'total' a subtração de 'créditos' e 'débitos'
+      // Os 3 valores são usados para preencher o 'value-box'
+      vm.total = vm.credit - vm.debt
     }
 
     vm.refresh()
--- backend/config/server.js
+++ backend/config/server.js
@@ -4,25 +4,15 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
 const allowCors = require('./cors')
+const queryParser = require('express-query-int') // Talvez não seja necessário
 
 server.use(bodyParser.urlencoded({ extend: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
+server.use(queryParser()) // Talvez não seja necessário
 
 server.listen(port, function(){
   console.log(`BACKEND is running on port ${port}.`)
 })
 
 module.exports = server
-
-/*
-server.use(function(req, res, next){
-  console.log('meu middleware 1');
-  next()
-})
-
-server.use(function(req, res, next){
-  console.log('meu middleware 2');
-  res.send('Funcionou novamente....')
-})
-*/
